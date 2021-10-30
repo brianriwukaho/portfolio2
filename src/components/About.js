31722ff9f92bcb78eaebf60128a9e3e963b1179a
brianriwukaho <b.riwukaho@gmail.com>
2021-10-30 10:48:37 +1000

    added logos in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,47 @@
 import React from "react";
-import me from "./me.jpg";
+import JS from "./js.svg";
+import TS from "./ts.svg";
+import JAVA from "./java.svg";
+import NODEJS from "./nodejs.svg";
+import AWS from "./aws.svg";
+import Cfn from "./cloudformation.svg";
+import Docker from "./docker.svg";
+import Git from "./git.svg";
+import ReactLogo from "./reactlogo.svg";
+import Kubernetes from "./kubernetes.svg";
+
 export default function About() {
   return (
     <div className="about-container">
+      <div className="about-title">
+        <div className="about-title-name">About Me</div>
+        <h1 className="about-title-intro">
+          As a kid I played with legos. In my teens I played minecraft. <br /> I
+          still do those things, but now I code as well. <br />
+          Same vibe you feel?
+        </h1>
+      </div>
       <div className="about-content-container">
-        <div className="about-title-container">
-          <div className="about-title">
-            <div className="about-title-name">About Me</div>
-            <h1 className="about-title-intro">
-              As a kid I played with legos.
-              <br />
-              <br />
-              In my teens I played minecraft.
-              <br />
-              <br />
-              I still do those things,
-              <br />
-              <br />
-              but now I code as well.
-              <br />
-              <br />
-              Same vibe you feel?
-            </h1>
+        <div className="about-skills-container">
+          <div className="languages">
+            <h2>Languages</h2>
+            <div className="icon-container">
+              <JS />
+              <TS />
+              <JAVA />
+              <NODEJS />
+            </div>
+          </div>
+          <div className="tools">
+            <h2>Tools</h2>
+            <div className="icon-container">
+              <ReactLogo />
+              <Docker />
+              <Kubernetes />
+              <AWS />
+              <Cfn />
+              <Git />
+            </div>
           </div>
         </div>
         <div className="about-info">
